366a9da39464155c8ec0ef19f81ac7204cc13732
ericsgagnon <ericsgagnon@gmail.com>
2022-12-22 21:08:58 +0000

    WIP Add support for common interfaces

--- null/nullable.go
+++ null/nullable.go
@@ -1,6 +1,9 @@
 package null
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type Nullable[T any] struct {
 	V     T
@@ -14,10 +17,14 @@ func New[T any](v T) Nullable[T] {
 	}
 }
 
+func FromPointer[T any](pointer *T) Nullable[T] {
+	return New(*pointer)
+}
+
 func (n *Nullable[T]) Set(v T) {
 	var x Nullable[T]
 	if i := (interface{}(v)); i != nil {
-		x = New[T](v)
+		x = New(v)
 	}
 	*n = x
 }
@@ -28,6 +35,66 @@ func (n Nullable[T]) Get() T {
 	}
 	return *new(T)
 }
+
+func (n Nullable[T]) MarshalJSON() ([]byte, error) {
+	if !n.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(n.V)
+}
+
+// TODO: write UnmarshalJSON
+// func (n *Nullable[T]) UnmarshalJSON(b []byte) error {
+
+// 	return nil
+// }
+
+// TODO: is there a general purpose scan function like json.Unmarshal?
+// func (n *Nullable[T]) Scan(src any) error {
+// 	if src == nil {
+// 		n.V = *new(T)
+// 		n.Valid = false
+// 		return nil
+// 	}
+// 	*n = New()
+
+// 	return nil
+// }
+
+// TODO: write Value
+// func (n Nullable[T]) Value() (driver.Value, error) {
+// 	return nil, nil
+// }
+
+func (n Nullable[T]) MarshalText() ([]byte, error) {
+	if !n.Valid {
+		return []byte{}, nil
+	}
+	return []byte(fmt.Sprint(n.V)), nil
+}
+
+// TODO: is there a general purpose text unmarshaling function like json.Unmarshal
+// func (n *Nullable[T]) UnmarshalText(b []byte) error {
+// 	return nil
+// }
+
+// should probably leave gostring alone
+// func (n Nullable[T]) GoString() string {
+// 	if !n.Valid {
+// 		return "null"
+// 	}
+// 	return fmt.Sprintf("%#v", n.Value)
+// }
+
+func (n Nullable[T]) String() string {
+	if !n.Valid {
+		return "null"
+	}
+	return fmt.Sprint(n.V)
+}
+
+// func (n *Nullable[T]) ()
+// func (n *Nullable[T]) ()
 
 func DispatchTest[T any](v T) {
 	switch (interface{}(v)).(type) {
